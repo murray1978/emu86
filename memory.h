13c3fd7ff6c8d169333f40be0177422620c1dde6
murray1978 <muzzdeni78@gmail.com>
2014-05-03 22:00:12 +1200

    Update memory.h
    Reformatting of comments, added boot rom lookup to get_phys_bytes().
    Added TODO's in code.

--- memory.h
+++ memory.h
@@ -4,17 +4,27 @@
 #include "cpu.h"
 
 /*
- Memory Bounderies
+ Jump Rom Memory.
 */
 #define JMP_START 0xffff0
 #define JMP_END   0xfffff
 
+/*
+ Boot Rom
+*/
 #define ROM_SIZE  0x64
 #define ROM_START JMP_START - ROM_SIZE
 #define ROM_END   JMP_START
 
+/*
+ Interupt vector table RAM
+*/
 #define IVT_START 0
 #define IVT_END   0x3ff
+
+/*
+ System Ram, could be external EEPROM, FLash etc.
+*/
 #define RAM_START 0x400
 #define RAM_SIZE  0xff
 #define RAM_END   RAM_START + RAM_SIZE
@@ -39,19 +49,26 @@ byte mem_rom256[] = {
   0x64, 0x64, 0x64, 0x64, 0x64, 0x64, 0x64, 0x64, 0x64, 0x64,
 };
 
-//15bytes to make a jump,
+
+/* 
+ 15bytes to make a jump,
+ */
 byte jmp_rom[] = {
-  // mov ax, ROM_START      184, 256, 256, ( 0xb8, 0xff, 0xff)
-  // immediate to register, word, 
-  // mov cs, ax             142, 200, ( 0x8E, 0xC8 )
-  // reg to segment        
-  // jmp 0                 233, 249 (0xE9, 0xF9)             
-  //reset ip.
+  /*
+   Test code for jmp rom
+     mov ax, ROM_START      184, 256, 256, ( 0xb8, 0xff, 0xff)
+     immediate to register, word, 
+     mov cs, ax             142, 200, ( 0x8E, 0xC8 )
+     reg to segment        
+     jmp 0                 233, 249 (0xE9, 0xF9)             
+    reset ip.
+  */
   0xB8, 0xff, 0xff, 0x8E, 0xC8, 
   0xE9, 0xF9, 0x64, 0x00, 0x00, 
   0x00, 0x00, 0x64, 0x64, 0xff, 
   0xff
 };
+
 /*
   use up about 1k of precious ram
   64k segments.
@@ -80,30 +97,54 @@ mem_t mem_rom256 = {
 }*/
 
 /*
+  Return a byte of memory from the loction provided by the Code Segment and Instruction Pointers.
 */
 byte get_phys_byte( unsigned int ip){
   unsigned long mem = ( unsigned long )cpu86.regs.cs;
   mem = (mem << 4);
   mem += ip;
   
-  //is memory in IVT ?
+  /*
+   is memory in IVT ?
+  */
   if( mem <= IVT_END ){
     if( mem >= 0){
        return mem_low1024[mem];   
     }
   }
   
-  //is the location in jmp rom, last address to check
+  /*
+    TODO
+    Is the location in external eeprom memory
+  */
+  
+  
+  /*
+   test for location mem_rom256, at location ROM_START
+  */
+  if( mem >= ROM_START){
+  	if( mem < ROM_END ){
+  	 	if( ( mem - ROM_START ) < ROM_SIZE ){ 
+  			return (byte)mem_rom256[mem - ROMSTART];
+  	 	}
+  	}
+  }
+  
+  /*
+   is the location in jmp rom, last address to check, at location 0xffff0 - 0xfffff
+  */
   if( mem >= JMP_START ){
     if( mem <= JMP_END ){
-         mem -= JMP_START;
-         if( mem < 16 ){
-           return (byte)jmp_rom[mem];
+         if( ( mem - JMP_START ) < 16 ){
+           return (byte)jmp_rom[mem - JMP_START];
          }
       }
     }
   
-  //else a noop, should be invalid memory
+  /*
+    Return a noop86 instruction.
+    TODO confirm we can use 0x0f as a noop.
+  */
   return 0x0f;
 }
 
